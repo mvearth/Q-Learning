aec1600a1d0b52b037c4a582c01f1baf37a263fb
mathvterra <mathvterra@gmail.com>
2019-06-09 21:08:43 -0300

    Improving the algorithm

---
+++ agent.js
@@ -0,0 +1,4 @@
+const NORTH = 0;
+const SOUTH = 1;
+const EAST = 2;
+const WEST = 3;
--- q_learning.js
+++ q_learning.js
@@ -1,136 +1,229 @@
-const NORTH = 0
-const SOUTH = 1
-const EAST = 2
-const WEST = 3
+const NORTH = 0;
+const SOUTH = 1;
+const EAST = 2;
+const WEST = 3;
 
-const BLACK_SPOTS = [7, 10, 14, 19, 20, 21, 24, 27, 30, 31, 37, 39, 40, 41]
-const DESIRED_STATE = 50
+const BLACK_SPOTS = [7, 10, 14, 19, 20, 21, 24, 27, 30, 31, 37, 39, 40, 41];
+const DESIRED_STATE = 50;
 
 class Coordinate {
     constructor(x, y) {
-        this.X = x
-        this.Y = y
+        this.X = x;
+        this.Y = y;
     }
 }
 
-var state = startCoordenate()
-var q = startQ()
-var map = startMap()
+var q = startQ();
+var grid = startMap();
+
+prinQ();
+
+var numEpisodes = 100;
+var maxStepsPerEpisode = 100;
+var learningRate = 0.5;
+var discountRate = 0.99;
+
+var explorationRate = 0.3
 
 function startCoordenate() {
-    return new Coordinate(0, 0)
+    return new Coordinate(0, 0);
 }
 
 function startQ() {
-    var newQ = Array(51)
+    var newQ = Array(51);
 
-    for (i = 1; i <= 50; i++)
-        newQ[i] = [0, 0, 0, 0]
+    for (i = 1; i <= 50; i++) {
+        newQ[i] = [0, 0, 0, 0];
+    }
 
-    return newQ
+    return newQ;
 }
 
-function prinQ(){
+function prinQ() {
     for (i = 1; i <= 50; i++)
-        console.log(q[i])
+        console.log(q[i]);
 }
 
 function startMap() {
-    var matrix = [], height = 6, width = 11
+    var matrix = [];
+
+    var maxY = 5;
+    var x = 0;
+    var y = 0;
+    var direction = 1;
+    var value = 1;
 
     for (var y = 0; y < 5; y++) {
-        matrix[y] = new Array(10)
+        matrix[y] = new Array(10);
     }
-    var maxY = 5, x = y = 0, direction = value = 1
+
+    y = 0;
 
     while (value < 51) {
         if (y == maxY || y == -1) {
-            direction *= -1
-            x++
-            y += direction
+            direction *= -1;
+            x++;
+            y += direction;
         }
         else {
-            matrix[y][x] = value
-            y += direction
-            value++
+            matrix[y][x] = value;
+            y += direction;
+            value++;
         }
     }
 
-    return matrix
+    return matrix;
 }
 
-// for (var y = 0; y < 10; y++) {
-//     for (var x = 0; x < 5; x++) {
-//         console.log(map[x][y])
-//     }
-// }
-
 function move(currentCoordinate, direction) {
-    var x, y
+    var x;
+    var y;
 
     switch (direction) {
         case NORTH:
-            y = currentCoordinate.Y + 1
-            x = currentCoordinate.X
-            break
+            y = currentCoordinate.Y + 1;
+            x = currentCoordinate.X;
+            break;
 
         case SOUTH:
-            y = currentCoordinate.Y - 1
-            x = currentCoordinate.X
-            break
+            y = currentCoordinate.Y - 1;
+            x = currentCoordinate.X;
+            break;
 
         case EAST:
-            x = currentCoordinate.X + 1
-            y = currentCoordinate.Y
-            break
+            x = currentCoordinate.X + 1;
+            y = currentCoordinate.Y;
+            break;
 
         case WEST:
-            x = currentCoordinate.X - 1
-            y = currentCoordinate.Y
-            break
+            x = currentCoordinate.X - 1;
+            y = currentCoordinate.Y;
+            break;
     }
 
-    return new Coordinate(x, y)
+    return new Coordinate(x, y);
 }
 
 function getReward(currentCoordinate) {
     if (currentCoordinate.X < 0 || currentCoordinate.Y < 0 || currentCoordinate.Y > 4 || currentCoordinate.X > 9)
-        return -100
+        return -100;
 
-    var state = map[currentCoordinate.Y][currentCoordinate.X]
+    var state = grid[currentCoordinate.Y][currentCoordinate.X];
 
     if (BLACK_SPOTS.includes(state))
-        return -100
+        return -100;
 
     if (state === DESIRED_STATE)
-        return 100
+        return 100;
 
-    return -1
+    return -1;
 }
 
-function action(currentCoordinate, direction) {
-    var currentState = map[currentCoordinate.Y][currentCoordinate.X]
+function takeOneStep(currentCoordinate, action) {
+    var nextCoordinate = move(currentCoordinate, action);
 
-    if (currentState === DESIRED_STATE) {
-        return getReward(currentCoordinate)
-    }
-    else {
-        var nextDirection = 2
+    var reward = getReward(nextCoordinate);
 
-        var nextCoordinate = move(currentCoordinate, nextDirection)
+    var nextState = currentCoordinate;
 
-        var reward = action(nextCoordinate, nextDirection)
+    if (nextCoordinate.Y < grid.length && nextCoordinate.X < grid[nextCoordinate.Y].le)
+        nextState = grid[nextCoordinate.Y][nextCoordinate.X];
+    else
+        nextCoordinate = currentCoordinate;
 
-        var maxReward = Math.max(...q[currentState])
-
-        var result = reward  + 0.5 * maxReward
-
-        q[currentState][direction] = reward
-
-        return result
-    }
+    return { nextCoorditate: nextCoordinate, reward: reward, done: nextState === DESIRED_STATE };
 }
 
-action(state, 2)
-prinQ()
+function range(start, end) {
+    var ans = [];
+    for (let i = start; i <= end; i++) {
+        ans.push(i);
+    }
+    return ans;
+}
 
+function getRandomAction(currentCoordinate) {
+    var actionRangeEnd = 4;
+    var possibleActions = [0, 1, 2, 3]
+
+    if (currentCoordinate.X === 0 && currentCoordinate.Y === 0) {
+        possibleActions = [0, 2];
+        actionRangeEnd = 2;
+    }
+    else if (currentCoordinate.X === 0 && currentCoordinate.Y === 4) {
+        possibleActions = [1, 2];
+        actionRangeEnd = 2;
+    }
+    else if (currentCoordinate.X === 9 && currentCoordinate.Y === 4) {
+        possibleActions = [1, 3];
+        actionRangeEnd = 2;
+    }
+    else if (currentCoordinate.X === 9 && currentCoordinate.Y === 0) {
+        possibleActions = [0, 3];
+        actionRangeEnd = 2;
+    }
+    else if (currentCoordinate.Y === 0) {
+        possibleActions = [0, 2, 3];
+        actionRangeEnd = 3;
+    }
+    else if (currentCoordinate.X === 0) {
+        possibleActions = [0, 1, 2];
+        actionRangeEnd = 3;
+    }
+    else if (currentCoordinate.Y === 4) {
+        possibleActions = [1, 2, 3];
+        actionRangeEnd = 3;
+    }
+    else if (currentCoordinate.X === 9) {
+        possibleActions = [0, 1, 3];
+        actionRangeEnd = 3;
+    }
+
+    return possibleActions[Math.floor(Math.random() * actionRangeEnd)];
+}
+
+for (episode in range(1, numEpisodes)) {
+    var coordinate = startCoordenate();
+
+    var state = grid[coordinate.Y][coordinate.X];
+
+    var done = false;
+
+    for (step in range(1, maxStepsPerEpisode)) {
+
+        var action;
+
+        explorationRateThreshold = Math.random();
+
+        if (explorationRateThreshold > explorationRate) {
+            var maxValue = Math.max.apply(null, q[state])
+
+            for (var i = 0; i < q[state].length; i++) {
+                if (q[state][i] === maxValue)
+                    action = i;
+                break;
+            }
+        }
+        else
+            action = getRandomAction(coordinate);
+
+        var stepResult = takeOneStep(coordinate, action);
+
+        var nextState = grid[stepResult.nextCoorditate.Y][stepResult.nextCoorditate.X];
+
+        var reward = stepResult.reward;
+
+        done = stepResult.done;
+
+        q[state, action] = q[state][action] + learningRate * Math.max.apply(null, q[nextState]);
+
+        coordinate = stepResult.nextCoorditate;
+
+        if (done)
+            break;
+    }
+
+    console.log(episode)
+}
+
+prinQ();
